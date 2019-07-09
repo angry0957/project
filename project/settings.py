4c8195cb9d6ea8074250657a153840d73a2c25f4
Abdul Rahman <abdul.095722@gmail.com>
2019-07-10 00:15:07 +0500

    Updated

--- project/settings.py
+++ project/settings.py
@@ -154,7 +154,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # import datetime
 # JWT_AUTH = {
 #     'JWT_ENCODE_HANDLER':
