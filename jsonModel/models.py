577eb9d7b7c9c65e19aff2138083dd9430230e77
Abdul Rahman <abdul.095722@gmail.com>
2019-08-15 21:43:24 +0500

    Updated

--- jsonModel/models.py
+++ jsonModel/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-
+from django.utils.timezone import now
 # Create your models here.
 
 class JsonModel(models.Model):
     name = models.CharField(max_length=200)
     data = JSONField()
+    date = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
     
     def __str__(self):
         """A string representation of the model."""
